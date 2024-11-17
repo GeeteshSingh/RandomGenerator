8971476d23e4617c47062fe7a9f30dd1b6000778
Geetesh Singh <you@example.com>
2024-11-18 00:40:01 +0530

    fixed with new API

--- src/App.js
+++ src/App.js
@@ -1,37 +1,17 @@
-import './App.css';
+import React, { useState } from "react";
 import RandomJoke from "./RandomJoke";
-import {useRef, useState} from "react";
-function reload() {
-    window.location.refresh();
-}
 
 function App() {
-    const firstNameRef = useRef(null);
-    const lastNameRef = useRef(null);
-    const [firstName, setFirstName] = useState('Geetesh');
-    const [lastName, setLastName] = useState('Singh');
-    const joke = RandomJoke(firstName, lastName);
+    const [firstName, setFirstName] = useState("First");
+    const [lastName, setLastName] = useState("Last");
 
-    const generateJoke = (e) => {
-        e.preventDefault()
-        setFirstName(firstNameRef.current.value)
-        setLastName(lastNameRef.current.value)
-    }
+    const handleFetchJoke = () => {
+    };
+
     return (
-        <div className="App">
-            <h1>Joke Generator</h1>
-            <h2>{joke}</h2>
-            <div className='jokeSection'>
-                <form>
-                    <input placeholder='First name' ref={firstNameRef}/>
-                    <input placeholder='Last name' ref={lastNameRef}/>
-                    <br/>
-                    <button className='button' onClick={generateJoke} >Generate</button>
-                    <button className='button' onClick={reload}>New Joke</button>
-
-                </form>
-            </div>
-
+        <div className="container">
+            <h1 style={{textAlign:"center"}}>Random Joke</h1>
+            <RandomJoke firstName={firstName} lastName={lastName} />
         </div>
     );
 }
--- src/RandomJoke.js
+++ src/RandomJoke.js
@@ -1,17 +1,40 @@
-import {useEffect,useState} from "react";
+import { useEffect, useState } from "react";
+import "./App.css"
+function RandomJoke() {
+    const [firstName, setFirstName] = useState("John");
+    const [lastName, setLastName] = useState("Cena");
+    const [joke, setJoke] = useState("");
 
-function RandomJoke(firstName,lastName){
-    const [joke, setJoke] = useState('')
+    const fetchJoke = async () => {
+        const response = await fetch("https://api.chucknorris.io/jokes/random");
+        const data = await response.json();
+        setJoke(data.value.replace(/Chuck Norris/g, `${firstName} ${lastName}`));
+    };
+
     useEffect(() => {
-        const fetchJoke = async () =>
-            await fetch(`http://api.icndb.com/jokes/random?firstName=${firstName}&lastName=${lastName}`)
-                .then((response) => response.json())
-                .then((data) => {
-                    setJoke(data.value.joke)
-                })
-        fetchJoke();
-    }, [firstName,lastName])
-return joke;
+        if (firstName && lastName) {
+            fetchJoke().then(r => r.json()).then(data => setFirstName(data));
+        }
+    }, [firstName, lastName]);
+
+    return (
+        <div className="joke-app">
+            <input
+                type="text"
+                placeholder="Enter your first name"
+                value={firstName}
+                onChange={(e) => setFirstName(e.target.value)}
+            />
+            <input
+                type="text"
+                placeholder="Enter your last name"
+                value={lastName}
+                onChange={(e) => setLastName(e.target.value)}
+            />
+            <button onClick={fetchJoke}>Get Joke</button>
+            <h2>{joke}</h2>
+        </div>
+    );
 }
 
-export default RandomJoke
+export default RandomJoke;
